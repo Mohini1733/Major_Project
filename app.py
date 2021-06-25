45cdb9baa0dfeb03070b1960532faf70f947619f
Mohini Deshpande <62890793+Mohini1733@users.noreply.github.com>
2021-06-25 19:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
 
 import streamlit as st
 import joblib
-import base64
-st.set_page_config(layout="wide")
-st.markdown("""
-<style>
-.big-font {
-    font-size;50px  !important;
-    font-family:Arial;
-    color:#D5C4C4; 
-
-}
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-<style>
-.big-font {
-    font-size;20px  !important;
-    font-family:Courier;
-    color:#D5C4C4; 
-}
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown('<p class="big-font">IMDB Review Sentiment Analysis !!</p>',
-            unsafe_allow_html=True)
-
-st.markdown('<p class="medium-font">Enter your review!</p>', 
-            unsafe_allow_html=True)
-
-review= st.text_input("here")
-model=joblib.load("imdb-rating")
-
-op=model.predict([review])
-
-if st.button('Analyse'):
-  st.markdown(f'<p class="medium-font">The Review is {op[0]} </p>',
-              unsafe_allow_html=True)
+model = joblib.load('IMDB_Reviews')
+st.title('Sentiment Analyser')
+ip = st.text_input('Enter your review: ')
+op = model.predict([ip])
+ans=op[0]
+if st.button('Predict'):
+  st.title(op[0])
